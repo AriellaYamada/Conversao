cf5462e29113be00e90755cb0240f82ef5854019
AriellaYamada <ariella.y.b@hotmail.com>
2015-05-13 19:58:03 -0300

    GG CABÔ

--- T1.asm
+++ T1.asm
@@ -6,6 +6,7 @@ strNum : .asciiz "\nDigite o numero: "
 strBaseOut : .asciiz "Digite a base do numero de saida[B, D, H, O]: "
 strEnter : .asciiz "\n"
 strOctal : .asciiz "0o"
+strInvalid : .asciiz "\nEntrada Invalida, verifique a base selecionada.\n"
 
 .text
 
@@ -17,11 +18,7 @@ syscall
 #le o char da base de entrada
 li $v0, 12	#read char
 syscall
-add $t0, $zero, $v0	#armazena o char lido em $t0
-
-#li $v0, 11	#print char
-#move $a0, $t3
-#syscall
+add $s0, $zero, $v0	#armazena o char lido em $s0
 
 #printa a string strNum
 li $v0, 4	#print string
@@ -29,19 +26,19 @@ la $a0, strNum
 syscall
 
 
-#comparacoes
+CompareIn:
 
 li $t3, 'B'	#carrega letra pra comparacao
-beq $t0, $t3, BIn
+beq $t3 $s0, BIn
 
 li $t3, 'D'
-beq $t3, $t0, DIn
+beq $t3, $s0, DIn
 
 li $t3, 'H' 
-beq $t3, $t0, HIn
+beq $t3, $s0, HIn
 
 li $t3, 'O'
-beq $t3, $t0, OIn
+beq $t3, $s0, OIn
 
 
 BIn:
@@ -51,6 +48,9 @@ BIn:
 	
 	li $t2, 0 #Inicializa resultado final
 	li $t4, 2 #base
+	
+	li $t3, 1 #Maior algarismo possivel em binario
+	
 	 
 	loopBIn:
 		
@@ -64,6 +64,10 @@ BIn:
 		mul $t2, $t2, $t4 	#multiplica o numero pela base
 		
 		subi $t0, $t0, '0'	#converte o caractere para decimal
+		
+		bltz $t0, ErrorMessage	#se valor digitado for menor que o minimo
+		bgt $t0, $t3, ErrorMessage	#se valor digitado for maior que o maximo
+		
 		add $t2, $t2, $t0	#soma o caractere digitado
 		
 		j loopBIn
@@ -77,14 +81,42 @@ BIn:
 		j Out
 
 DIn:
-	#Le decimal
-	li $v0, 5
-	syscall
+	addi $sp, $sp, -8
+	sw $a0, 0($sp)
+	sw $ra, 4($sp)
 	
-	#Armazena numero lido
-	add $t2, $zero, $v0
+	li $t2, 0 #Inicializa resultado final
+	li $t4, 10 #base
 	
-	j Out
+	li $t3, 9	#maior algarismo possivel em decimal
+	 
+	loopDIn:
+		
+		li $v0, 12	#read char
+		syscall
+		add $t0, $zero, $v0	#armazena o char lido em $t0
+		
+		li $t1, '\n'
+		beq $t0, $t1, loopDInEnd
+	
+		mul $t2, $t2, $t4 	#multiplica o numero pela base
+		
+		subi $t0, $t0, '0'	#converte o caractere para decimal
+		
+		bltz $t0, ErrorMessage	#se valor digitado for menor que o minimo
+		bgt $t0, $t3, ErrorMessage	#se valor digitado for maior que o maximo
+		
+		add $t2, $t2, $t0	#soma o caractere digitado
+		
+		j loopDIn
+				
+	loopDInEnd:
+		
+		lw $a0, 0($sp)
+		lw $ra, 4($sp)
+		addi $sp, $sp, 8
+		
+		j Out
 
 HIn:
 	addi $sp, $sp, -8
@@ -113,14 +145,26 @@ HIn:
 		
 		loopHIn09:
 		
+			li $t3, 9	#maior algarismo possivel ate 9 em hexadecimal
+			
 			subi $t0, $t0, '0'	#converte o caractere para decimal
+			
+			bltz $t0, ErrorMessage	#se valor digitado for menor que o minimo
+			bgt $t0, $t3, ErrorMessage	#se valor digitado for maior que o maximo
+			
 			add $t2, $t2, $t0	#soma o caractere lido
 			
 			j loopHIn
 		
 		loopHInaf:
 			
-			subi $t0, $t0, '0'	#converte o caractere para decimal (valor de 0 a 5)
+			li $t3, 5	#maior algarismo possivel ate f em hexadecimal (5 = f)
+			
+			subi $t0, $t0, 'a'	#converte o caractere para decimal (valor de 0 a 5)
+			
+			bltz $t0, ErrorMessage	#se valor digitado for menor que o minimo
+			bgt $t0, $t3, ErrorMessage	#se valor digitado for maior que o maximo
+			
 			addi $t0, $t0, 10	#soma 10 (valor de 10 a 15)
 			add $t2, $t2, $t0	#soma o caractere lido
 			
@@ -141,6 +185,8 @@ OIn:
 	
 	li $t2, 0 #Inicializa resultado final
 	li $t4, 8 #base
+	
+	li $t3, 7	#maior algarismo possivel em octal
 	 
 	loopOIn:
 		
@@ -155,6 +201,8 @@ OIn:
 		
 		
 		subi $t0, $t0, '0'	#converte o caractere para decimal
+		bltz $t0, ErrorMessage	#se valor digitado for menor que o minimo
+		bgt $t0, $t3, ErrorMessage	#se valor digitado for maior que o maximo
 		add $t2, $t2, $t0	#soma o caractere lido
 			
 		j loopOIn
@@ -271,3 +319,13 @@ End:
 
 li $v0, 10
 syscall		#termina programa
+
+ErrorMessage:
+	
+	#print strInvalid
+	li $v0, 4	#print string
+	la $a0, strInvalid
+	syscall
+	
+	j CompareIn
+	
