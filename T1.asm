f2c952d024c7ffb70eeefa50fd358b98acd6907d
AriellaYamada <ariella.y.b@hotmail.com>
2015-05-16 22:32:34 -0300

    TA QUASE, falta apenas o hexa

--- T1.asm
+++ T1.asm
@@ -5,14 +5,18 @@
 .data
 .align 0
 
-strBaseIn : .asciiz "Digite a base do numero a ser inserido[B, D, H, O]: "
+strBaseIn : .asciiz "\nDigite a base do numero a ser inserido[B, D, H, O] ou 'S' para sair: "
 strNum : .asciiz "\nDigite o numero: "
-strBaseOut : .asciiz "Digite a base do numero de saida[B, D, H, O]: "
+strBaseOut : .asciiz "\nDigite a base do numero de saida[B, D, H, O]: "
 strEnter : .asciiz "\n"
+strBin : .asciiz "0b"
 strOctal : .asciiz "0o"
+strHex : .asciiz "0x"
 strInvalid : .asciiz "\nEntrada Invalida, verifique a base selecionada.\n"
 
 .text
+
+Inicio:
 
 #printa a string strBaseIn
 li $v0, 4	#print string
@@ -44,6 +48,10 @@ beq $t3, $s0, HIn
 li $t3, 'O'
 beq $t3, $s0, OIn
 
+li $t3, 'S'
+beq $t3, $s0, End
+
+#j ErrorMessage
 
 BIn:
 	addi $sp, $sp, -8
@@ -54,9 +62,11 @@ BIn:
 	li $t4, 2 #base
 	
 	li $t3, 1 #Maior algarismo possivel em binario
-	
+	li $t5, 32 #Maior nymero de dígitos possíveis
 	 
 	loopBIn:
+		
+		beqz $t5, loopBInEnd
 		
 		li $v0, 12	#read char
 		syscall
@@ -73,6 +83,8 @@ BIn:
 		bgt $t0, $t3, ErrorMessage	#se valor digitado for maior que o maximo
 		
 		add $t2, $t2, $t0	#soma o caractere digitado
+		
+		subi $t5, $t5, 1
 		
 		j loopBIn
 				
@@ -129,8 +141,11 @@ HIn:
 	
 	li $t2, 0 #Inicializa resultado final
 	li $t4, 16 #base
+	li $t5, 8
 	 
 	loopHIn:
+		
+		beqz $t5, loopHInEnd
 		
 		li $v0, 12	#read char
 		syscall
@@ -158,6 +173,8 @@ HIn:
 			
 			add $t2, $t2, $t0	#soma o caractere lido
 			
+			subi $t5, $t5, 1
+			
 			j loopHIn
 		
 		loopHInaf:
@@ -171,6 +188,8 @@ HIn:
 			
 			addi $t0, $t0, 10	#soma 10 (valor de 10 a 15)
 			add $t2, $t2, $t0	#soma o caractere lido
+			
+			subi $t5, $t5, 1
 			
 			j loopHIn
 				
@@ -191,8 +210,11 @@ OIn:
 	li $t4, 8 #base
 	
 	li $t3, 7	#maior algarismo possivel em octal
+	li $t5, 11
 	 
 	loopOIn:
+		
+		beqz $t5, loopOInEnd
 		
 		li $v0, 12	#read char
 		syscall
@@ -209,6 +231,8 @@ OIn:
 		bgt $t0, $t3, ErrorMessage	#se valor digitado for maior que o maximo
 		add $t2, $t2, $t0	#soma o caractere lido
 			
+		subi $t5, $t5, 1
+		
 		j loopOIn
 		
 				
@@ -258,29 +282,64 @@ beq $t3, $t0, OOut
 
 
 BOut:
-
-	li $v0, 35	#print decimal em binario
-	move $a0, $s1
+	addi $sp, $sp, -8
+	sw $a0, 0($sp)
+	sw $ra, 4($sp)
+	
+	move $a0, $s1	#carrega o numero
+	li $a1, 2	#carrega a base
+	jal CountDig	#retorna o numero de digitos em $s2
+	
+	#aloca espaco na memoria para a string
+	li $v0, 9	#sbrk
+	add $a0, $zero, $s2	#tamanho
 	syscall
 	
-	j End	
+	move $s3, $v0	#endereco da memoria alocada
+	add $s3, $s3, $s2	#soma o endereco com o tamanho da string
+	
+	li $t0, '\0'
+	sb $t0, ($s3)
+	
+		loopBOut:
+			
+			beqz $s1, loopBOutEnd	#se o numero for zero
+			
+			subi $s3, $s3, 1	#decrementa o valor do endereco da string
+			
+			andi $t0, $s1, 1	# num mod 2
+			
+			addi $t0, $t0, '0'	#transforma em char
+			sb $t0, ($s3)	#armazena o digito no final da string
+			
+			sra $s1, $s1, 1 	#divide o numero por 2
+			
+			j loopBOut
+			
+loopBOutEnd:
+	
+	li $v0, 4
+	la $a0, strBin
+	syscall
+
+	move $a1, $s3	#move a string para $a1
+	jal printStr
+	
+	lw $a0, 0($sp)
+	lw $ra, 4($sp)
+	addi $sp, $sp, 8
+	
+	j Inicio	
 
 DOut:
 
-	li $v0, 1	#print decimal em decimal
+	li $v0, 36	#print decimal em decimal
 	move $a0, $s1
 	syscall
 	
-	j End	
+	j Inicio	
 
 HOut:
-	li $v0, 34	#print decimal em hexadecimal
-	move $a0, $s1
-	syscall
-	
-	j End	
-
-OOut:
 	addi $sp, $sp, -8
 	sw $a0, 0($sp)
 	sw $ra, 4($sp)
@@ -290,11 +349,11 @@ OOut:
 	li $t5, 10	#base
 	
 
-		loopOOut:
+		loopHOut:
 			
-			beqz $s1, loopOOutEnd	#se o numero for zero
+			beqz $s1, loopHOutEnd	#se o numero for zero
 			
-			andi $t3, $s1, 7	# num mod 7
+			andi $t3, $s1, 15	# num mod 15
 			
 			mul $t3, $t3, $t4	#multiplica o mod pela base
 			
@@ -302,23 +361,75 @@ OOut:
 
 			add $t2, $t2, $t3	#adiciona o mod no numero final
 			
+			sra $s1, $s1, 4 	#divide o numero por 16
+			
+			j loopHOut
+			
+	loopHOutEnd:	
+		lw $a0, 0($sp)
+		lw $ra, 4($sp)
+		addi $sp, $sp, 8
+	
+		move $s1, $t2	#carrega o resultado em octal para $s1
+	
+		li $v0, 4
+		la $a0, strHex
+		syscall
+	
+		j DOut	
+
+OOut:
+	addi $sp, $sp, -8
+	sw $a0, 0($sp)
+	sw $ra, 4($sp)
+	
+	move $a0, $s1	#carrega o numero
+	li $a1, 8	#carrega a base
+	jal CountDig	#retorna o numero de digitos em $s2
+	
+	#aloca espaco na memoria para a string
+	li $v0, 9	#sbrk
+	add $a0, $zero, $s2	#tamanho
+	syscall
+	
+	move $s3, $v0	#endereco da memoria alocada
+	add $s3, $s3, $s2	#soma o endereco com o tamanho da string
+	
+	li $t0, '\0'
+	sb $t0, ($s3)
+	
+		loopOOut:
+			
+			beqz $s1, loopOOutEnd	#se o numero for zero
+			
+			subi $s3, $s3, 1	#decrementa o valor do endereco da string
+			
+			andi $t0, $s1, 7	# num mod 7
+			
+			addi $t0, $t0, '0'	#transforma em char
+			sb $t0, ($s3)	#armazena o digito no final da string
+			
 			sra $s1, $s1, 3 	#divide o numero por 8
 			
 			j loopOOut
 			
-loopOOutEnd:	
+loopOOutEnd:
+	
+	move $s1, $t2	#carrega o resultado em octal para $s1
+	
+	li $v0, 4
+	la $a0, strOctal
+	syscall
+
+	move $a1, $s3	#move a string para $a1
+	jal printStr
+	
 	lw $a0, 0($sp)
 	lw $ra, 4($sp)
 	addi $sp, $sp, 8
 	
-	move $s1, $t2	#carrega o resultado em octal para $s1
+	j Inicio
 	
-	li $v0, 4	#print string
-	la $a0, strOctal
-	syscall
-	
-	j DOut
-
 End:
 
 li $v0, 10
@@ -332,4 +443,87 @@ ErrorMessage:
 	syscall
 	
 	j CompareIn
+	
+CountDig:
+
+	#recebe o numero em $a0 e a base em $a1
+	#retorna o numero de digitos em $s2
+
+	addi $sp, $sp, -12
+	sw $a1, 0($sp)
+	sw $a0, 4($sp)
+	sw $ra, 8($sp)
+	
+	move $t0, $a0	#copia o valor entrado
+	move $t1, $a1	#copia o valor da base
+	li $s2, 0	#inicializa o contador de digito
+	
+	loopCountDig:
+		beqz $t0, loopCountDigEnd
+		
+		div $t0, $t0, $t1	#divide o numero pela base
+		addi $s2, $s2, 1	#incrementa contador
+		
+		j loopCountDig
+		
+		loopCountDigEnd:
+	
+			addi $s2, $s2, 1	#espaco para o \0 da string
+			lw $ra, 8($sp)
+			lw $a0, 4($sp)
+			lw $a1, 0($sp)
+			addi $sp, $sp, 12
+	
+			jr $ra
+
+printStr:
+
+	addi $sp, $sp, -12
+	sw $a1, 0($sp)
+	sw $a0, 4($sp)
+	sw $ra, 8($sp)
+
+	li $t0, '\0'
+	
+		loopPrintStr:
+			lb $t1, ($s3)
+			
+			beq $t1, $t0 loopPrintStrEnd
+			
+			li $v0, 11	#print char
+			move $a0, $t1
+			syscall
+			
+			addi $s3, $s3, 1	#incrementa a posicao de memoria
+			
+			j loopPrintStr
+			
+			loopPrintStrEnd:
+				
+				lw $ra, 8($sp)
+				lw $a0, 4($sp)
+				lw $a1, 0($sp)
+				addi $sp, $sp, 12
+	
+				jr $ra
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
